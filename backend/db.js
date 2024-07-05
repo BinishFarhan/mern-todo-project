14725f36bcecd526fcd47b7acaca53c74b8e861c
Binish <fullstack786.dev01@gmail.com>
2024-07-05 11:14:56 +0500

    .env created

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose')
 
-async function dbconnect(){
-// database connection
-mongoose.connect("mongodb+srv://binishfarhan89:vamp1989@cluster0.vzdef1c.mongodb.net/mern_todo")
-}
+// async function dbconnect(){
+// // database connection
+// mongoose.connect("mongodb+srv://binishfarhan89:password@cluster0.vzdef1c.mongodb.net/mern_todo")
+// }
 
+// module.exports = dbconnect
+
+async function dbconnect() {
+    // database connection
+    mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Database Connected"))
+    .catch((err) => console.log(err))
+    }
 module.exports = dbconnect
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT 
 const cors = require('cors')
 const todoRouter = require('./routes/todos')
 const dbconnect = require('./db')
 
+
+require('dotenv').config({ path: '.env' })
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
